335cea2e5e666666239d28919b4e82f60808dc37
Arthur <arthurhsilvaaraujo@gmail.com>
2024-10-21 11:53:08 -0300

    Fiz a tela de editar perfil

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './src/Screens/LoginScreen';
 import HomeScreen from './src/Screens/HomeScreen';
 import LoadingScreen from './src/Screens/LoadingScreen';
+import EditProfile from './src/Screens/EditProfile';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Loading" component={LoadingScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
