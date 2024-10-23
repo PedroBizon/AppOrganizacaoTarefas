5190a2f9d24dcf87a1124d52edb7673bf9ed1622
Pedro Bizon <pedrodania@gmail.com>
2024-10-23 20:34:10 -0300

    Pre merge

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { colors } from './src/constants/colors';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.blue,
+    backgroundColor: colors.white,
     alignItems: 'center',
     justifyContent: 'center',
   },
